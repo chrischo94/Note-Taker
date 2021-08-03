e7d6879758c16b33fd96c09f896e0304d5ebbfe2
chrischo94 <82658603+chrischo94@users.noreply.github.com>
2021-08-04 02:35:17 +0900

    readme

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# Note-Taker
+# Note Taker ![MIT](https://img.shields.io/badge/license-MIT-red)
+
+  ## Description 
+  A place to store notes
+
+  ## Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * 
+  ## Installation
+  run npm i, then npm run start
+
+  ## Usage 
+  take notes
+
+  ## License
+  This product uses a MIT license
+
+  ## Demo
+  <img src="assets/images/ezgif.com-gif-maker(3).gif">
+
+  ## Questions
+  For additional questions feel free to contact: chrischo2012@gmail.com
+  GitHub: https://github.com/chrischo94
+  
--- server.js
+++ server.js
@@ -3,14 +3,36 @@ const path = require('path');
 const fs = require("fs");
 const app = express();
 const PORT = process.env.PORT || 3001;
-
+app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+const notes = require('./db/db.json');
 
-require('./routes/apiRoutes')(app);
-require('./routes/htmlRoutes')(app);
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'))
+})
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'))
+})
 
+app.get('/api/notes', (req, res) => {
+    res.json(notes)
+})
+app.post('/api/notes', (req, res) =>{
+    const note = req.body
+    note.id - notes.length.toString()
+    notes.push(note)
+    fs.writeFileSync('./db/db.json', JSON.stringify(notes));
+    res.json(note)
+})
+app.delete('/api/notes/:id', (req, res)=> {
+    notes = notes.filter(note => {
+        return note.id !== req.params.id
+    })
+        fs.writeFileSync('./db/db.json', JSON.stringify(notes))
+        res.json(notes)
+})
 app.listen(PORT, () => {
-    console.log(`App listening on PORT: ${3001}`);
+    console.log(`App listening on PORT: ${PORT}`);
   });
   
