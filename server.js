0e171deb73fcc2fc2a735668f2d09b11f42c941c
chrischo94 <82658603+chrischo94@users.noreply.github.com>
2021-08-04 01:29:59 +0900

    file structure and server.js rough

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const path = require('path');
+const fs = require("fs");
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+require('./routes/apiRoutes')(app);
+require('./routes/htmlRoutes')(app);
+
+app.listen(PORT, () => {
+    console.log(`App listening on PORT: ${3001}`);
+  });
+  
